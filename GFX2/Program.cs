2403778e474c4ba7d526d06c0983bda165cbd729
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-10-21 10:45:09 +0200

    commit L

--- GFX2/Program.cs
+++ GFX2/Program.cs
@@ -11,8 +11,15 @@ namespace GFX2
 
             Color myColor = new Color(0, 0, 0, 0);
 
+            //left Y pos
             float LY = 0;
+            //left height
+            int LH = 80;
+
+            //right y pos
             float RY = 0;
+            //right height
+            int RH = 80;
 
             float ballX = 400;
             float ballY = 300;
@@ -21,14 +28,39 @@ namespace GFX2
             //Så länge fönstret inte ska stängas; fortsätt while
             while(!Raylib.WindowShouldClose())
             {
+                //Ball movement
+
+                //Left movement
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+                {
+                    if (LY >= 0)
+                    {
+                        LY = LY - 0.5f;
+                    }
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+                {
+                    if (LY <= 800 - LH)
+                    {
+                        LY = LY + 0.5f;
+                    }
+                    
+                }
+
+                //Right movement
+
 
                 Raylib.BeginDrawing();
 
                 Raylib.ClearBackground(myColor);
 
-                Raylib.DrawRectangle(2, (int)LY, 10, 30, Color.WHITE);
+                Raylib.DrawCircle((int)ballX, (int)ballY, 15, Color.WHITE);
 
-                Raylib.DrawRectangle(2, (int)RY, 40, 40, Color.RED);
+                Raylib.DrawRectangle(2, (int)LY, 10, LH, Color.WHITE);
+
+                Raylib.DrawRectangle(788, (int)RY, 10, RH, Color.WHITE);
 
                 Raylib.EndDrawing();
             }
